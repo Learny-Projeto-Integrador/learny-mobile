4635f694f705c793a8e695b8c28cee80e3f86d1c
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-02-24 20:28:54 -0300

    Adicionando a fonte montserrat

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import { Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
@@ -23,8 +24,8 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    Montserrat_400Regular,
+    Montserrat_700Bold,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
