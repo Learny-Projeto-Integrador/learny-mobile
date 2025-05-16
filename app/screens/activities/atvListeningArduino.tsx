4b282e81048641c033cb34024830cff16f070194
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-05-15 22:32:35 -0300

    fix: ajustando verificação do botão
    Ajustando verificação do botão com useFocusEffect e useCallBack para a verificação da rota ser puxada apenas quando está na tela da fase

--- app/screens/activities/atvListeningArduino.tsx
+++ app/screens/activities/atvListeningArduino.tsx
@@ -67,13 +67,13 @@ export default function AtvListeningArduinoScreen() {
   const [medalha, setMedalha] = useState<string | null>(null);
   const [audio, setAudio] = useState<any>(null);
   const [infoVisible, setInfoVisible] = useState<boolean>(false);
-  const [botaoPressionado, setBotaoPressionado] = useState("")
+  const [botaoPressionado, setBotaoPressionado] = useState("");
 
   const { getDuration } = useScreenDuration();
   const { submitMission } = useSubmitMission();
   const { checkMedalha } = useCheckMedalha();
   const { checkAudio } = useCheckAudio();
-  
+
   useFocusEffect(
     useCallback(() => {
       const fetchMedalha = async () => {
@@ -206,32 +206,35 @@ export default function AtvListeningArduinoScreen() {
     }
   };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      fetch('http://10.0.2.2:5000/button')
-        .then(res => res.json())
-        .then(data => {
-          console.log(data.button)
-          if (!data.button || shuffledOptions.length < 3 || !correctOption) return;
-  
-          const buttonMap: { [key: string]: AudioOption } = {
-            ok1: shuffledOptions[0],
-            ok2: shuffledOptions[1],
-            ok3: shuffledOptions[2],
-          };
-  
-          const selectedOption = buttonMap[data.button];
-  
-          if (!selectedOption) return;
-  
-          const isCorrect = selectedOption.id === correctOption.id;
-          handleConfirm(isCorrect);
-        })
-        .catch(err => console.log(err));
-    }, 1000);
-  
-    return () => clearInterval(interval);
-  }, [shuffledOptions, correctOption]);
+  useFocusEffect(
+    useCallback(() => {
+      const interval = setInterval(() => {
+        fetch("http://10.0.2.2:5000/button")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data.button)
+            if (!data.button || shuffledOptions.length < 3 || !correctOption)
+              return;
+
+            const buttonMap: { [key: string]: AudioOption } = {
+              ok1: shuffledOptions[0],
+              ok2: shuffledOptions[1],
+              ok3: shuffledOptions[2],
+            };
+
+            const selectedOption = buttonMap[data.button];
+
+            if (!selectedOption) return;
+
+            const isCorrect = selectedOption.id === correctOption.id;
+            handleConfirm(isCorrect);
+          })
+          .catch((err) => console.log(err));
+      }, 1000);
+
+      return () => clearInterval(interval); // Limpa o intervalo ao sair da tela
+    }, [shuffledOptions, correctOption])
+  );
 
   if (!correctOption) return null;
 
@@ -319,10 +322,7 @@ export default function AtvListeningArduinoScreen() {
         <Text style={styles.txtPergunta}>Que animal é esse?</Text>
         <View style={{ flexDirection: "row", gap: width * 0.05 }}>
           {shuffledOptions.map((option) => (
-            <View
-              key={option.id}
-              style={styles.retanguloAnimal}
-            >
+            <View key={option.id} style={styles.retanguloAnimal}>
               <Text style={styles.txtEmocao}>{option.label}</Text>
             </View>
           ))}
