a1854a43eb4e55e3c6621eb5dce9e3258c2c2303
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-09-03 19:26:37 -0300

    feat: adiciona novos tipos no type.ts

--- types.ts
+++ types.ts
@@ -1,26 +1,45 @@
-// types.ts
 export type RootStackParamList = {
-    index: undefined;
-    register: { idParent?: string };
-    transition: { name: string, type: string };
-    profileParent: undefined;
-    edit: { userFilho?: string }
-    home: undefined;
-    profileChildren: undefined;
-    iconChildren: undefined;
-    world: undefined;
-    menu: undefined;
-    diary: undefined;
-    ranking: undefined;
-    atvFeeling: undefined;
-    atvMatch: undefined;
-    atvMatchAnswer: { score: {}, answer: {}};
-    atvListening: undefined;
-    atvMemory: undefined;
-    atvConnect: undefined;
-    score: { score: {} };
-    scoreFail: { score: {} };
-  };
+  index: undefined;
+  register: { idParent?: string };
+  transition: { name: string, type: string };
+  profileParent: undefined;
+  edit: { userFilho?: string }
+  home: undefined;
+  profileChildren: undefined;
+  iconChildren: undefined;
+  world: undefined;
+  menu: undefined;
+  diary: undefined;
+  ranking: undefined;
+  atvFeeling: undefined;
+  atvMatch: undefined;
+  atvMatchAnswer: { score: {}, answer: {}};
+  atvListening: undefined;
+  atvMemory: undefined;
+  atvConnect: undefined;
+  score: { score: {} };
+  scoreFail: { score: {} };
+};
+
+export type ParentData = {
+  idParent: string;
+  foto: string;
+  nome: string;
+  filhos: [{}];
+  filhoSelecionado: {fasesConcluidas: any};
+};
+
+export type AlertProps = {
+  icon: any;
+  visible: boolean;
+  title: string;
+  message: string;
+  dualAction?: boolean;
+  onClose: () => void;
+  onRedirect?: () => void; // ação do segundo botão
+  closeLabel?: string;
+  redirectLabel?: string; // texto do segundo botão
+};
 
 export type AlertData = {
   icon?: any;
@@ -28,10 +47,11 @@ export type AlertData = {
   message: string;
   dual?: boolean;
   label?: string;
+  redirectLabel?: string;
+  onRedirect?: () => void;
   score?: {
     pontos: number;
     porcentagem: number;
     tempo: string;
   };
 }
-  
