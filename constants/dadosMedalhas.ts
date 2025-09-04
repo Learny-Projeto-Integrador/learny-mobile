2ab94624398b12845eadcaa304ee7e9197e995de
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-09-04 18:16:04 -0300

    feat: adiciona constants para medalhas e missões

---
+++ constants/dadosMedalhas.ts
@@ -0,0 +1,5 @@
+export const imgMedalhas: any = {
+  "Iniciando!": require("@/assets/icons/medalha-verde-select.png"),
+  "A todo o vapor!": require("@/assets/icons/medalha-vermelha-select.png"),
+  "Desvendando": require("@/assets/icons/medalha-azul-select.png"),
+};
---
+++ constants/dadosMissoes.ts
@@ -0,0 +1,9 @@
+import { ImageSourcePropType } from "react-native";
+
+export const imgsMissoes: Record<string, ImageSourcePropType> = {
+  "uma fase": require("@/assets/images/diarias/diaria-uma-fase.png"),
+  "fase connect": require("@/assets/images/diarias/diaria-ligar.png"),
+  "fase listening": require("@/assets/images/diarias/diaria-escuta.png"),
+  "fase feeling": require("@/assets/images/diarias/diaria-emocoes.png"),
+  "fase memoria": require("@/assets/images/diarias/diaria-memoria.png"),
+};
