32273e6a4631e9f1010789c95f589ce6142812c1
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-09-04 19:47:41 -0300

    feat: adiciona dados para constants
    Foi adicionado dados para a constante de medalhas e foi criada a constante para dados de fases

--- constants/dadosMedalhas.ts
+++ constants/dadosMedalhas.ts
@@ -3,3 +3,24 @@ export const imgMedalhas: any = {
   "A todo o vapor!": require("@/assets/icons/medalha-vermelha-select.png"),
   "Desvendando": require("@/assets/icons/medalha-azul-select.png"),
 };
+
+export const efeitos: any = {
+  "Iniciando!": "+50 pontos por fase",
+  "A todo o vapor!": "pontos x2 nas fases",
+  "Desvendando": "habilita as dicas nas fases",
+};
+
+export const cores: any = {
+  "Iniciando!": {
+    corEscura: "#34A103",
+    corClara: "#80D25B",
+  },
+  "A todo o vapor!": {
+    corEscura: "#CB4653",
+    corClara: "#DE7E88",
+  },
+  "Desvendando": {
+    corEscura: "#439FC7",
+    corClara: "#6CD2FF",
+  },
+};
