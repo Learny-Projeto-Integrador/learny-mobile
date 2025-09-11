3b93bc36ca386bf93a910cba086c7616c97f5411
João Marcos Alecsandro Kirimis <150600077+jmkirimis@users.noreply.github.com>
2025-09-10 21:52:28 -0300

    refactor: implementa fila de alertas no context

--- contexts/AlertContext.tsx
+++ contexts/AlertContext.tsx
@@ -10,35 +10,52 @@ const AlertContext = createContext<AlertContextType | undefined>(undefined);
 
 export function useCustomAlert() {
   const context = useContext(AlertContext);
-  if (!context) {
-    throw new Error("useAlert must be used within AlertProvider");
-  }
+  if (!context) throw new Error("useAlert must be used within AlertProvider");
   return context;
 }
 
 type Props = { children: ReactNode };
 
 export function AlertProvider({ children }: Props) {
-  const [alertData, setAlertData] = useState<AlertData | null>(null);
+  const [queue, setQueue] = useState<AlertData[]>([]);
+  const [current, setCurrent] = useState<AlertData | null>(null);
   const [visible, setVisible] = useState(false);
 
   const showAlert = (data: AlertData) => {
-    setAlertData(data);
-    setVisible(true);
+    setQueue(prev => [...prev, data]);
+    // se não tiver alerta atual, mostra imediatamente
+    if (!current) {
+      setCurrent(data);
+      setVisible(true);
+    }
+  };
+
+  const handleClose = () => {
+    setVisible(false);
+    setQueue(prevQueue => {
+      const [, ...rest] = prevQueue; // remove o primeiro
+      if (rest.length > 0) {
+        setCurrent(rest[0]);
+        setVisible(true);
+      } else {
+        setCurrent(null);
+      }
+      return rest;
+    });
   };
 
   const AlertComponent = () =>
-    alertData ? (
+    current ? (
       <CustomAlert
-        icon={alertData.icon}
+        icon={current.icon}
         visible={visible}
-        dualAction={alertData.dualAction || false}
-        title={alertData.title}
-        message={alertData.message}
-        closeLabel={alertData.closeLabel}
-        redirectLabel={alertData.redirectLabel}
-        onClose={() => setVisible(false)}
-        onRedirect={alertData.onRedirect}
+        dualAction={current.dualAction || false}
+        title={current.title}
+        message={current.message}
+        closeLabel={current.closeLabel}
+        redirectLabel={current.redirectLabel}
+        onClose={handleClose}
+        onRedirect={current.onRedirect}
       />
     ) : null;
 
